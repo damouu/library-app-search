58e0b74a267dbca1f2c49e67786dc04f157fa1b0
damouu <mouadsehbaoui@gmail.com>
2026-09-10 11:45:45 +0900

    Implement: search_handler.go (#LAA-5)

--- internal/handler/search_handler.go
+++ internal/handler/search_handler.go
@@ -1,3 +1,37 @@
 package handler
 
-//todo CONTROLLER LAYER
+import (
+	"encoding/json"
+	"library-app-search/internal/domain"
+	"net/http"
+)
+
+type SearchUseCase interface {
+	SearchChapters(query string) ([]domain.Chapter, error)
+}
+
+type SearchHandler struct {
+	searchUseCase SearchUseCase
+}
+
+func NewSearchHandler(searchUseCase SearchUseCase) *SearchHandler {
+	return &SearchHandler{searchUseCase: searchUseCase}
+}
+
+func (s *SearchHandler) Search(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("q")
+	if query == "" {
+		http.Error(w, "400 Bad Request query parameter is required", http.StatusBadRequest)
+		return
+	}
+	chapters, err := s.searchUseCase.SearchChapters(query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(chapters)
+	if err != nil {
+		return
+	}
+}
