9fbbee55e36c0b8e6f604ae6923fad28bb6326ae
damouu <mouadsehbaoui@gmail.com>
2026-09-13 19:31:14 +0900

    Feature: router.go (#LAA-7)

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,16 +3,19 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"library-app-search/internal/handler"
 	"library-app-search/internal/health"
 )
 
-func NewRouter(h *health.Handler) chi.Router {
+func NewRouter(h *health.Handler, searchHandler *handler.SearchHandler) chi.Router {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
 	router.Get("/health/live", health.LiveHandler)
 
 	router.Get("/health/ready", h.ReadyHandler)
+
+	router.Get("/search", searchHandler.Search)
 
 	return router
 
